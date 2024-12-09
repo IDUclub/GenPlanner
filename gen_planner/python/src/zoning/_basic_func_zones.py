0a1e1246c046d5f2a7ed1e7442b6d357e6f728c9
DDonny <63115678+DDonnyy@users.noreply.github.com>
2024-12-09 22:45:14 +0300

    duplicated 09.12.24 (fix) - basic func_zones and terr_zones name fix

--- gen_planner/python/src/zoning/_basic_func_zones.py
+++ gen_planner/python/src/zoning/_basic_func_zones.py
@@ -1,2 +1,2 @@
-from .standart_implement import residential_territory, industrial_territory, business_territory, recreation_territory, \
-    transport_territory, agricalture_territory, special_territory
+from .standart_implement import residential_func_zone, industrial_func_zone, business_func_zone, recreation_func_zone, \
+    transport_func_zone, agricalture_func_zone, special_func_zone
--- gen_planner/python/src/zoning/standart_implement.py
+++ gen_planner/python/src/zoning/standart_implement.py
@@ -2,124 +2,124 @@ from .models import TerritoryZone, FuncZone, GenPlan
 
 minimum_block_area = 160000
 
-residential = TerritoryZone(
+residential_terr = TerritoryZone(
     "residential",
     minimum_block_area,
 )
-industrial = TerritoryZone(
+industrial_terr = TerritoryZone(
     "industrial",
     minimum_block_area * 4,
 )
-business = TerritoryZone(
+business_terr = TerritoryZone(
     "business",
     minimum_block_area,
 )
-recreation = TerritoryZone(
+recreation_terr = TerritoryZone(
     "recreation",
     minimum_block_area * 2,
 )
-transport = TerritoryZone(
+transport_terr = TerritoryZone(
     "transport",
     minimum_block_area,
 )
-agriculture = TerritoryZone(
+agriculture_terr = TerritoryZone(
     "agriculture",
     minimum_block_area * 4,
 )
-special = TerritoryZone(
+special_terr = TerritoryZone(
     "special",
     minimum_block_area,
 )
 
-basic_scenario = FuncZone(
+basic_func_zone = FuncZone(
     {
-        residential: 0.25,
-        industrial: 0.12,
-        business: 0.08,
-        recreation: 0.3,
-        transport: 0.1,
-        agriculture: 0.03,
-        special: 0.02,
+        residential_terr: 0.25,
+        industrial_terr: 0.12,
+        business_terr: 0.08,
+        recreation_terr: 0.3,
+        transport_terr: 0.1,
+        agriculture_terr: 0.03,
+        special_terr: 0.02,
     },
     "basic",
 )
 
-residential_territory = FuncZone(
+residential_func_zone = FuncZone(
     {
-        residential: 0.5,
-        business: 0.1,
-        recreation: 0.1,
-        transport: 0.1,
-        agriculture: 0.05,
-        special: 0.05,
+        residential_terr: 0.5,
+        business_terr: 0.1,
+        recreation_terr: 0.1,
+        transport_terr: 0.1,
+        agriculture_terr: 0.05,
+        special_terr: 0.05,
     },
     "residential territory",
 )
 
-industrial_territory = FuncZone(
+industrial_func_zone = FuncZone(
     {
-        industrial: 0.5,
-        business: 0.1,
-        recreation: 0.05,
-        transport: 0.1,
-        agriculture: 0.05,
-        special: 0.05,
+        industrial_terr: 0.5,
+        business_terr: 0.1,
+        recreation_terr: 0.05,
+        transport_terr: 0.1,
+        agriculture_terr: 0.05,
+        special_terr: 0.05,
     },
     "industrial territory",
 )
-business_territory = FuncZone(
+business_func_zone = FuncZone(
     {
-        residential: 0.1,
-        business: 0.5,
-        recreation: 0.1,
-        transport: 0.1,
-        agriculture: 0.05,
-        special: 0.05,
+        residential_terr: 0.1,
+        business_terr: 0.5,
+        recreation_terr: 0.1,
+        transport_terr: 0.1,
+        agriculture_terr: 0.05,
+        special_terr: 0.05,
     },
     "business territory",
 )
-recreation_territory = FuncZone(
+recreation_func_zone = FuncZone(
     {
-        residential: 0.2,
-        business: 0.1,
-        recreation: 0.5,
-        transport: 0.05,
-        agriculture: 0.1,
+        residential_terr: 0.2,
+        business_terr: 0.1,
+        recreation_terr: 0.5,
+        transport_terr: 0.05,
+        agriculture_terr: 0.1,
     },
     "recreation territory",
 )
-transport_territory = FuncZone(
+transport_func_zone = FuncZone(
     {
-        industrial: 0.1,
-        business: 0.05,
-        recreation: 0.05,
-        transport: 0.5,
-        agriculture: 0.05,
-        special: 0.05,
+        industrial_terr: 0.1,
+        business_terr: 0.05,
+        recreation_terr: 0.05,
+        transport_terr: 0.5,
+        agriculture_terr: 0.05,
+        special_terr: 0.05,
     },
     "transport territory",
 )
-agricalture_territory = FuncZone(
+agricalture_func_zone = FuncZone(
     {
-        residential: 0.1,
-        industrial: 0.1,
-        business: 0.05,
-        recreation: 0.1,
-        transport: 0.05,
-        agriculture: 0.5,
-        special: 0.05,
+        residential_terr: 0.1,
+        industrial_terr: 0.1,
+        business_terr: 0.05,
+        recreation_terr: 0.1,
+        transport_terr: 0.05,
+        agriculture_terr: 0.5,
+        special_terr: 0.05,
     },
     "agriculture territory",
 )
-special_territory = FuncZone(
+special_func_zone = FuncZone(
     {
-        residential: 0.01,
-        industrial: 0.1,
-        business: 0.05,
-        recreation: 0.05,
-        transport: 0.05,
-        agriculture: 0.05,
-        special: 0.5,
+        residential_terr: 0.01,
+        industrial_terr: 0.1,
+        business_terr: 0.05,
+        recreation_terr: 0.05,
+        transport_terr: 0.05,
+        agriculture_terr: 0.05,
+        special_terr: 0.5,
     },
     "special territory",
 )
@@ -127,12 +127,12 @@ special_territory = FuncZone(
 gen_plan = GenPlan(
     name="General Plan",
     func_zone_ratio={
-        recreation_territory: 0.333,
-        residential_territory: 0.277,
-        industrial_territory: 0.133,
-        transport_territory: 0.111,
-        business_territory: 0.088,
-        agricalture_territory: 0.033,
-        special_territory: 0.022,
+        recreation_func_zone: 0.333,
+        residential_func_zone: 0.277,
+        industrial_func_zone: 0.133,
+        transport_func_zone: 0.111,
+        business_func_zone: 0.088,
+        agricalture_func_zone: 0.033,
+        special_func_zone: 0.022,
     }
 )
