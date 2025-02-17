bc151d6dea1f697a29428ffeaf588a7d45f790b3
DDonnyy <63115678+ddonnyy@users.noreply.github.com>
2025-02-17 18:23:08 +0300

    17.02.2025

--- app/gen_planner/python/src/genplanner/genplanner.py
+++ app/gen_planner/python/src/genplanner/genplanner.py
@@ -58,10 +58,16 @@ class GenPlanner:
         task_queue = multiprocessing.Queue()
         task_queue.put((initial_func, args, kwargs))
         res, roads = parallel_split_queue(task_queue, self.local_crs)
+
         if self.rotation:
             res.geometry = res.geometry.apply(
                 lambda x: Polygon(rotate_coords(x.exterior.coords, self.pivot_point, self.angle_rad_to_rotate))
             )
+
+        if roads.empty:
+            return res, roads
+
+        if self.rotation:
             roads.geometry = roads.geometry.apply(
                 lambda x: LineString(rotate_coords(x.coords, self.pivot_point, self.angle_rad_to_rotate))
             )
@@ -99,37 +105,37 @@ class GenPlanner:
             polygon_splitter, self.transformed_poly, zones_ratio_dict, roads_width, local_crs=self.local_crs
         )
 
-    def poly2block(self, terr_zone: TerritoryZone) -> (gpd.GeoDataFrame, gpd.GeoDataFrame):
+    def poly2block(self, terr_zone: TerritoryZone) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
         if self.source_multipolygon:
             raise NotImplementedError("Multipolygon source is not supported yet")
         return self._run(poly2block_initial, self.transformed_poly, terr_zone, local_crs=self.local_crs)
 
-    def poly2terr(self, funczone: FuncZone = basic_func_zone) -> (gpd.GeoDataFrame, gpd.GeoDataFrame):
+    def poly2terr(self, funczone: FuncZone = basic_func_zone) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
         if self.source_multipolygon:
             return self._run(
                 multipoly2terr2block_initial, self.transformed_poly, funczone, False, local_crs=self.local_crs
             )
         return self._run(poly2terr2block_initial, self.transformed_poly, funczone, False, local_crs=self.local_crs)
 
-    def poly2terr2block(self, funczone: FuncZone = basic_func_zone) -> (gpd.GeoDataFrame, gpd.GeoDataFrame):
+    def poly2terr2block(self, funczone: FuncZone = basic_func_zone) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
         if self.source_multipolygon:
             return self._run(
                 multipoly2terr2block_initial, self.transformed_poly, funczone, True, local_crs=self.local_crs
             )
         return self._run(poly2terr2block_initial, self.transformed_poly, funczone, True, local_crs=self.local_crs)
 
-    def poly2func(self, genplan: GenPlan = gen_plan) -> (gpd.GeoDataFrame, gpd.GeoDataFrame):
+    def poly2func(self, genplan: GenPlan = gen_plan) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
         if self.source_multipolygon:
             raise NotImplementedError("Multipolygon source is not supported yet")
         return self._run(poly2func2terr2block_initial, self.transformed_poly, genplan, False, local_crs=self.local_crs)
 
-    def poly2func2terr2block(self, genplan: GenPlan = gen_plan) -> (gpd.GeoDataFrame, gpd.GeoDataFrame):
+    def poly2func2terr2block(self, genplan: GenPlan = gen_plan) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
         if self.source_multipolygon:
             raise NotImplementedError("Multipolygon source is not supported yet")
         return self._run(poly2func2terr2block_initial, self.transformed_poly, genplan, True, local_crs=self.local_crs)
 
 
-def parallel_split_queue(task_queue: multiprocessing.Queue, local_crs) -> (gpd.GeoDataFrame, gpd.GeoDataFrame):
+def parallel_split_queue(task_queue: multiprocessing.Queue, local_crs) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
     splitted = []
     roads_all = []
     with concurrent.futures.ProcessPoolExecutor() as executor:
@@ -156,7 +162,13 @@ def parallel_split_queue(task_queue: multiprocessing.Queue, local_crs) -> (gpd.G
             time.sleep(0.01)
             if not future_to_task and task_queue.empty():
                 break
+    roads_all = [roads_df for roads_df in roads_all if not (roads_df.is_empty())]
+    if len(roads_all) > 0:
+        roads_to_return = gpd.GeoDataFrame(pd.concat(roads_all, ignore_index=True), crs=local_crs, geometry="geometry")
+    else:
+        roads_to_return = gpd.GeoDataFrame()
+
     return (
         gpd.GeoDataFrame(pd.concat(splitted, ignore_index=True), crs=local_crs, geometry="geometry"),
-        gpd.GeoDataFrame(pd.concat(roads_all, ignore_index=True), crs=local_crs, geometry="geometry"),
+        roads_to_return,
     )
